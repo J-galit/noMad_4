3c105ab125b8c6ab5c59c40360be28342bce450d
itsjoooo <joline_la@hotmail.com>
2025-04-02 19:10:39 -0400

    start screen functional, bg art in

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,4 +8,10 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene("Act 1");
     }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("close game");
+    }
 }
